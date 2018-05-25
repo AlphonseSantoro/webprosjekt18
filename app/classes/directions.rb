43ae1e85c0b67116185eaa00fedcb4c9dede1bab
alphonsesantoro <kslagnes@mimer.no>
2018-05-25 11:00:58 +0200

    new button and icons on campus map

--- app/classes/directions.rb
+++ app/classes/directions.rb
@@ -1,11 +1,10 @@
 class Directions
 
-  def initialize(params, session)
-    locale = session ? 'en' : 'no'
+  def initialize(params, locale)
+    locale = locale == 'en' ? 'en' : 'no'
     to = params[:position].nil? ? params[:to] : params[:position]
     from = params[:location].nil? ? params[:from] : params[:location]
     url = URI("https://maps.googleapis.com/maps/api/directions/json?origin=#{from}&destination=#{to}&mode=transit&alternatives=true&language=#{locale}&key=#{ENV['GOOGLE_MAPS_API']}")
-    puts url
     @direction = JSON.parse(Net::HTTP.get(url))
   end
 
