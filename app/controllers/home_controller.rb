80995dfa92edc2c96e69d47bd1205618de7f28fd
Andy Long Nguyen <andylong47@me.com>
2018-05-28 12:55:34 +0200

    Lagt til veiviser fra vulkan til matbutikker/resturanter/hurtigmat

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,6 +33,26 @@ class HomeController < ApplicationController
   def vulkan
     @bikes = GoogleMaps.new(params).city_bikes({ lat: 59.923312, long: 10.752354 })
     @campus = 'Vulkan'
+    rema = {from: 'Vulkan+19+0178+oslo', to: 'Rema+1000+Vulkan, Maridalsveien+15, 0178+oslo'}
+    @rema = Directions.new(rema, session[:locale])
+    coopmega = {from: 'Vulkan+19+0178+oslo', to: 'Coop+Mega+Alexander+Kiellands, Waldemar+Thranes+gate 72, 0175+oslo'}
+    @coopmega = Directions.new(coopmega, session[:locale])
+    kiwifred = {from: 'Vulkan+19+0178+oslo', to: 'Kiwi+Fredensborg, Mollergata+56-58, 0179+Oslo'}
+    @kiwifred = Directions.new(kiwifred, session[:locale])
+    mathallen = {from: 'Vulkan+19+0178+oslo', to: 'Mathallen+Oslo, Vulkan 5, 0178+Oslo'}
+    @mathallen = Directions.new(mathallen, session[:locale])
+    dognvill = {from: 'Vulkan+19+0178+oslo', to: 'Kiwi+Fredensborg, Vulkan 12, 0178+Oslo'}
+    @dognvill = Directions.new(dognvill, session[:locale])
+    ferro = {from: 'Vulkan+19+0178+oslo', to: 'Ristorante+Ferro, Maridalsveien+13, 0175+Oslo'}
+    @ferro = Directions.new(ferro, session[:locale])
+    mcd = {from: 'Vulkan+19+0178+oslo', to: 'McDonalds, Thorvald+Meyers+gate+35-41, 0555+Oslo'}
+    @mcd = Directions.new(mcd, session[:locale])
+    grytelokket = {from: 'Vulkan+19+0178+oslo', to: 'McDonalds, Storgata+45, 0182+Oslo'}
+    @grytelokket = Directions.new(grytelokket, session[:locale])
+    subwayvul = {from: 'Vulkan+19+0178+oslo', to: 'Subway+grunerlokka, Seilduksgata+17, 0553+Oslo'}
+    @subwayvul = Directions.new(subwayvul, session[:locale])
+
+
   end
 
   def change_locale
